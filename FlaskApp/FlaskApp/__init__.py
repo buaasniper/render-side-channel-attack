0bc8286b119418b0ee76fb4e6c85bf6054710a1a
Ubuntu <ubuntu@ip-172-31-24-28.us-east-2.compute.internal>
2020-09-28 18:29:15 -0400

    finish send page wait code

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -38,21 +38,11 @@ num=10
 @app.route('/test_post/nn',methods=['GET','POST'])
 def test_post():
     global num
-    '''receive data'''
-    recv_data = request.get_data()
-    if recv_data:
-        print recv_data
-        json_re = json.loads(recv_data)
-        print json_re['email']
-        print json_re['phone']
-    else:
-        print("receive data is empty")
+   
  
     '''send data'''
     num = num + 1
-    testInfo['name'] = 'xiaoming'
-    testInfo['age'] = num
-    return json.dumps(testInfo)
+    return json.dumps(num)
 
 @app.route('/test_post/mm',methods=['GET','POST'])
 def test_post1():
